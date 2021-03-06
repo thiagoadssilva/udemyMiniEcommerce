031165a81a9dd588d5283bab707057eab3e79913
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-03-05 21:37:38 -0300

    ajustes no teste

--- frontend/src/components/Menu/menu.test.js
+++ frontend/src/components/Menu/menu.test.js
@@ -5,7 +5,7 @@ import Menu from '../Menu';
 
 describe('Teste do componente Menu', () =>{
     it('Deve redenrizar o componente sem erros', () =>{
-        const {getByText} = render(<Menu />);
+        const {getByText} = render(<Menu produtos={[]} handleExibirProdutos={() => false} handleExibirCheckout={() => false}/>);
         const texto = getByText(/menu/i)
         expect(texto).toBeInTheDocument();
     }) 
