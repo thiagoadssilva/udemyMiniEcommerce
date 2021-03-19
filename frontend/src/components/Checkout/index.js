1f00ffa4fd145826bd3014f2b81d52edce44fed8
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-03-18 21:24:01 -0300

    Implementando o formulario de cadstro

--- frontend/src/components/Checkout/index.js
+++ frontend/src/components/Checkout/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import {Form, Row, Col, Button, Jumbotron, Modal} from 'react-bootstrap';
+import 'react-datepicker/dist/react-datepicker';
+import DatePicker, {registerLocale} from 'react-datepicker';
+import pt from 'date-fns/locale/pt';
+
+registerLocale('pt', pt);
+
 import {
     Container
 } from './styled';
